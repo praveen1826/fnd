13333d9d86ded08fa6fa1d6ccd6c66adad4e3616
praveen1826 <thiruttanipraveen@gmail.com>
2022-11-06 22:55:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+//import { useState, useEffect, useRef } from "react";
+import * as tf from "@tensorflow/tfjs";
+import "./model.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "", outp: 1 };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.loadModel = this.loadModel.bind(this);
+  }
+  loadModel = async () => {
+    const model = await tf.loadGraphModel("./model.json");
+    //const outp1 = await model.classify(this.state.value);
+    const outp1 = model.predict(this.state.value);
+
+    this.setState({ outp: outp1 }, () => {
+      console.log(this.state.outp);
+    });
+  };
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A name was submitted: " + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" onClick={this.loadModel} />
+        <p>{this.state.outp}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
